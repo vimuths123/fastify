b1eecc8d2a24c1388ad143da76270e41c70b8c3b
vimuths123 <vimuths@yahoo.com>
2024-01-18 20:36:25 +0530

    get token

--- src/api/v1/controllers/loginController.ts
+++ src/api/v1/controllers/loginController.ts
@@ -15,7 +15,7 @@ export const sendOtp = async (request: FastifyRequest<SendOtpRequest>, reply: Fa
   const clientSecret = process.env.AUTH0_CLIENT_SECRET; 
   const userEmail = request.body.email;
 
-  
+
   try {
     const response = await axios.post(`${auth0Domain}/passwordless/start`, {
       client_id: clientId,
@@ -33,5 +33,41 @@ export const sendOtp = async (request: FastifyRequest<SendOtpRequest>, reply: Fa
   } catch (error) {
     console.error('Error sending OTP:', error);
     reply.code(500).send({ error: 'Error sending OTP' });
+  }
+};
+
+interface GetTokenRequest {
+  Body: {
+    email: string;
+    otp: string;
+  };
+}
+
+export const getToken = async (request: FastifyRequest<GetTokenRequest>, reply: FastifyReply) => {
+  const auth0Domain = process.env.AUTH0_DOMAIN;
+  const clientId = process.env.AUTH0_CLIENT_ID;
+  const clientSecret = process.env.AUTH0_CLIENT_SECRET;
+  const { email, otp } = request.body;
+
+  try {
+    const payload = {
+      grant_type: "http://auth0.com/oauth/grant-type/passwordless/otp",
+      client_id: clientId,
+      client_secret: clientSecret,
+      username: email,
+      otp: otp,
+      realm: "email",
+      audience: `${auth0Domain}/api/v2/`,
+      scope: "openid"
+    };
+
+    const response = await axios.post(`${auth0Domain}/oauth/token`, payload, {
+      headers: { 'Content-Type': 'application/json' }
+    });
+
+    reply.send(response.data);
+  } catch (error) {
+    console.error('Error getting token:', error);
+    reply.code(500).send({ error: 'Error getting token' });
   }
 };
--- src/api/v1/routes/loginRoutes.ts
+++ src/api/v1/routes/loginRoutes.ts
@@ -3,4 +3,5 @@ import * as loginController from '../controllers/loginController';
 
 export default async function (fastify: FastifyInstance) {
     fastify.post('/sendotp', loginController.sendOtp);
+    fastify.post('/gettoken', loginController.getToken);
 }
